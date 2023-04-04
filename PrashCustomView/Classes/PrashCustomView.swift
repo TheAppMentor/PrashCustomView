e5d0728e3df64bb55ce8aae0647a7958feba79e0
Prashanth <prashanth.moorthy@gmail.com>
2023-04-04 20:23:49 +0530

    Adding new complex ui

--- PrashCustomView/Classes/PrashCustomView.swift
+++ PrashCustomView/Classes/PrashCustomView.swift
@@ -13,9 +13,15 @@ public class PrashCustomView : UIView {
     public override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.magenta
+        self.frame = CGRect(origin: .zero, size: CGSize(width: 200, height: 75))
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        self.backgroundColor = UIColor.green
+        self.frame = CGRect(origin: .zero, size: CGSize(width: 200, height: 75))
+        
+        let shelfAuditView = Bundle.loadView(fromNib: "ShelfAuditCellView", withType: ShelfAuditCellView.self)
+        addSubview(shelfAuditView)
     }
 }
---
+++ PrashCustomView/Classes/ShelfAuditCellView.swift
@@ -0,0 +1,36 @@
+//
+//  ShelfAuditCellView.swift
+//  PrashCustomView
+//
+//  Created by Prashanth Moorthy on 03/04/23.
+//
+
+import UIKit
+
+public class ShelfAuditCellView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        let view = Bundle.loadView(fromNib: "ShelfAuditCellView", withType: ShelfAuditCellView.self)
+        view.addSubview(view)
+    }
+}
+
+extension Bundle {
+
+    static func loadView<T>(fromNib name: String, withType type: T.Type) -> T {
+        if let view = Bundle.main.loadNibNamed(name, owner: nil, options: nil)?.first as? T {
+            return view
+        }
+
+        fatalError("Could not load view with type " + String(describing: type))
+    }
+}
